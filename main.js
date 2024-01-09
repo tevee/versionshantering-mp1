5d58db41f7f6134e45edb873c1475566f06e6ff9
melsowole <82158719+melsowole@users.noreply.github.com>
2024-01-09 16:44:10 +0100

    added tabs feature

--- main.js
+++ main.js
@@ -1,42 +1,56 @@
-import { getRandomRecipes, getRecipesByIngredients, fetchData } from "./modules/fetchAPI.js"
-import { displaySearchedIngredients, removePrevIngredientSearch } from "./modules/display.js"
+import {
+	getRandomRecipes,
+	getRecipesByIngredients,
+	fetchData,
+} from "./modules/fetchAPI.js";
+import {
+	displaySearchedIngredients,
+	removePrevIngredientSearch,
+} from "./modules/display.js";
+import { handleTabClick } from "./modules/tabs.js";
 
-const ingredientsFormEl = document.querySelector('#getIngredientsForm')
-const searchRecipeFormEl = document.querySelector('#searchRecipe')
-const storedIngredientsContainerEl = document.querySelector('#storedIngredientsContainer')
+// eventlistener for tabs
+document.querySelector("nav").addEventListener("click", handleTabClick);
+
+const ingredientsFormEl = document.querySelector("#getIngredientsForm");
+const searchRecipeFormEl = document.querySelector("#searchRecipe");
+const storedIngredientsContainerEl = document.querySelector(
+	"#storedIngredientsContainer"
+);
 
 const savedResults = {
-    ingredients: [],
-}
+	ingredients: [],
+};
 
-ingredientsFormEl.addEventListener('submit', event => {
-    event.preventDefault();
-    removePrevIngredientSearch();
-    const inputElValue = document.querySelector('#getIngredientsForm > input').value
+ingredientsFormEl.addEventListener("submit", (event) => {
+	event.preventDefault();
+	removePrevIngredientSearch();
+	const inputElValue = document.querySelector(
+		"#getIngredientsForm > input"
+	).value;
 
-    savedResults.ingredients.push(inputElValue);
-    console.log(savedResults.ingredients);
+	savedResults.ingredients.push(inputElValue);
+	console.log(savedResults.ingredients);
 
-    displaySearchedIngredients(savedResults)
+	displaySearchedIngredients(savedResults);
 
-    ingredientsFormEl.reset()
-})
+	ingredientsFormEl.reset();
+});
 
-storedIngredientsContainerEl.addEventListener('click', event => {
-    event.preventDefault();
+storedIngredientsContainerEl.addEventListener("click", (event) => {
+	event.preventDefault();
 
-    savedResults.ingredients.forEach(ingredient => {
-        if(event.target.value === ingredient) {
-            const indexOfIngredient = savedResults.ingredients.indexOf(ingredient);
-            savedResults.ingredients.splice(indexOfIngredient, 1);
-            event.target.parentElement.remove();
+	savedResults.ingredients.forEach((ingredient) => {
+		if (event.target.value === ingredient) {
+			const indexOfIngredient = savedResults.ingredients.indexOf(ingredient);
+			savedResults.ingredients.splice(indexOfIngredient, 1);
+			event.target.parentElement.remove();
+		}
+	});
+});
 
-        }
-    })
-})
+searchRecipeFormEl.addEventListener("submit", (event) => {
+	event.preventDefault();
 
-searchRecipeFormEl.addEventListener('submit', event => {
-    event.preventDefault();
-
-    getRecipesByIngredients(savedResults)
-})
+	getRecipesByIngredients(savedResults);
+});
