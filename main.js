6e6f9298690d43a9d4c79b31e8c90c2372817c80
melsowole <82158719+melsowole@users.noreply.github.com>
2024-01-10 11:20:35 +0100

    fixed discover grid

--- main.js
+++ main.js
@@ -1,27 +1,40 @@
-import {getRandomRecipes,getRecipesByIngredients} from "./modules/fetchAPI.js";
-import {displaySearchedIngredients,removePrevIngredientSearch, displayError, removePrevRecipeSearch, displayRecipe, displayRecipeByIngredients} from "./modules/display.js";
+import {
+	getRandomRecipes,
+	getRecipesByIngredients,
+} from "./modules/fetchAPI.js";
+import {
+	displaySearchedIngredients,
+	removePrevIngredientSearch,
+	displayError,
+	removePrevRecipeSearch,
+	displayRecipe,
+	displayRecipeByIngredients,
+} from "./modules/display.js";
 import { handleTabClick } from "./modules/tabs.js";
-
 
 // eventlistener for tabs
 document.querySelector("nav").addEventListener("click", handleTabClick);
 
 const ingredientsFormEl = document.querySelector("#getIngredientsForm");
 const searchRecipeFormEl = document.querySelector("#searchRecipe");
-const storedIngredientsContainerEl = document.querySelector("#storedIngredientsContainer");
+const storedIngredientsContainerEl = document.querySelector(
+	"#storedIngredientsContainer"
+);
 
 const savedResults = {
 	ingredients: [],
 };
 
-// getRandomRecipes({number:5})
-// .then(displayRecipe)
-// .catch(error => displayError(error))
+getRandomRecipes({ number: 5 })
+	.then(displayRecipe)
+	.catch((error) => displayError(error));
 
 ingredientsFormEl.addEventListener("submit", (event) => {
 	event.preventDefault();
 	removePrevIngredientSearch();
-	const inputElValue = document.querySelector("#getIngredientsForm > input").value;
+	const inputElValue = document.querySelector(
+		"#getIngredientsForm > input"
+	).value;
 
 	savedResults.ingredients.push(inputElValue);
 	console.log(savedResults.ingredients);
@@ -34,35 +47,34 @@ ingredientsFormEl.addEventListener("submit", (event) => {
 storedIngredientsContainerEl.addEventListener("click", (event) => {
 	event.preventDefault();
 
-    savedResults.ingredients.forEach(ingredient => {
-        if(event.target.value === ingredient) {
-            const indexOfIngredient = savedResults.ingredients.indexOf(ingredient);
-            savedResults.ingredients.splice(indexOfIngredient, 1);
-            console.log(savedResults.ingredients);
-            event.target.parentElement.remove();
-        }
-    })
-})
+	savedResults.ingredients.forEach((ingredient) => {
+		if (event.target.value === ingredient) {
+			const indexOfIngredient = savedResults.ingredients.indexOf(ingredient);
+			savedResults.ingredients.splice(indexOfIngredient, 1);
+			console.log(savedResults.ingredients);
+			event.target.parentElement.remove();
+		}
+	});
+});
 
-searchRecipeFormEl.addEventListener('submit', event => {
-    event.preventDefault();
+searchRecipeFormEl.addEventListener("submit", (event) => {
+	event.preventDefault();
 
-    if(savedResults.ingredients.length !== 0) {
-        const displaySearchedIngredientsEl = searchRecipeFormEl.nextElementSibling
-        displaySearchedIngredientsEl.innerText = `Searched Ingredients: ${savedResults.ingredients}`
-        
-        getRecipesByIngredients(savedResults)
-        .then(results => {
-            removePrevIngredientSearch();
-            removePrevRecipeSearch();
-            savedResults.ingredients = [];
-            displayRecipeByIngredients(results)
-            
-        })
-        .catch(error =>{
-            displaySearchedIngredientsEl.innerText = '';
-            savedResults.ingredients = [];
-            displayError(error)
-        } )
-    }
-})
+	if (savedResults.ingredients.length !== 0) {
+		const displaySearchedIngredientsEl = searchRecipeFormEl.nextElementSibling;
+		displaySearchedIngredientsEl.innerText = `Searched Ingredients: ${savedResults.ingredients}`;
+
+		getRecipesByIngredients(savedResults)
+			.then((results) => {
+				removePrevIngredientSearch();
+				removePrevRecipeSearch();
+				savedResults.ingredients = [];
+				displayRecipeByIngredients(results);
+			})
+			.catch((error) => {
+				displaySearchedIngredientsEl.innerText = "";
+				savedResults.ingredients = [];
+				displayError(error);
+			});
+	}
+});
