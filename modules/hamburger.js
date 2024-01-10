483dbcad0d5de474b4885ee0d070c4e772772fd6
melsowole <82158719+melsowole@users.noreply.github.com>
2024-01-10 13:40:59 +0100

    made navigation functional

---
+++ modules/hamburger.js
@@ -0,0 +1,8 @@
+export function showSidebar() {
+	const sidebar = document.querySelector(".sidebar");
+	sidebar.style.display = "flex";
+}
+export function hideSidebar() {
+	const sidebar = document.querySelector(".sidebar");
+	sidebar.style.display = "none";
+}
