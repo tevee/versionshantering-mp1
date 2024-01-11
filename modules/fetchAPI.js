1b18d2676cb487e0c659cd34fb2e9c2930178b1c
melsowole <82158719+melsowole@users.noreply.github.com>
2024-01-11 15:06:58 +0100

    discover fixed

--- modules/fetchAPI.js
+++ modules/fetchAPI.js
@@ -31,29 +31,28 @@ export function getRecipesByIngredientsExtended(filters) {
 
 export function getRecipesInformation(ids) {
 	const END_POINT = "informationBulk";
-	
+
 	const URL = generateURL(END_POINT, ids);
-	
+
 	return fetchData(URL);
 }
 
-export function getInstructionsFromRecipeById(id){
-	const END_POINT = '/analyzedInstructions?'
+export function getInstructionsFromRecipeById(id) {
+	const END_POINT = "/analyzedInstructions?";
 
-	const URL = generateURL(END_POINT, '', id)
+	const URL = generateURL(END_POINT, "", id);
 
 	return fetchData(URL);
 }
 
 // API COMMUNICATION
 export async function fetchData(url) {
-
 	const response = await fetch(url);
 	const data = await response.json();
 
 	if (response.ok && data.length > 0) return data;
 	else if (data === undefined || data.length == 0) throw "emptyArray";
-	else if(response.ok && data.results.length > 0) return data; // data innehåller inte data.results när den vill ha data.recipes - felmeddelande fixa imorgon
+	else if (response.ok && data.length > 0) return data;
 	else if (response.ok && data.recipes.length > 0) return data;
 	else if (data.recipes.length === 0) throw "no recipes";
 	else throw "error";
@@ -61,20 +60,19 @@ export async function fetchData(url) {
 
 // HELPER FUNCTIONS
 function generateURL(endpoint, filters, id) {
-	// const API_KEY = "&apiKey=8aecc0a91be54e3c9cc4bfe9d6d468f2";
+	const API_KEY = "&apiKey=8aecc0a91be54e3c9cc4bfe9d6d468f2";
 	// 1cc618fa1481485e84da702af0191634 - Thiens API KEY
 	// 83d78591f91c440ead2234603cffd6c3 - Amandas API KEY
 	// deec58bb0fe24310940e44c32d429a87 - Andréas API KEY
 	// 543d189d1b144ef6899166c18c08621f - Elvira API KEY
-	const API_KEY = "&apiKey=83d78591f91c440ead2234603cffd6c3";
+	// const API_KEY = "&apiKey=83d78591f91c440ead2234603cffd6c3";
 	const BASE_URL = "https://api.spoonacular.com/recipes/";
 	const FILTER_PARAMS = getFilterString(filters);
 
-	if(id !== undefined) {
+	if (id !== undefined) {
 		const newAPI_KEY = API_KEY.substring(1);
 		return BASE_URL + id + endpoint + newAPI_KEY;
-	}
-	else return BASE_URL + endpoint + FILTER_PARAMS + API_KEY;
+	} else return BASE_URL + endpoint + FILTER_PARAMS + API_KEY;
 }
 
 function getFilterString(filters) {
