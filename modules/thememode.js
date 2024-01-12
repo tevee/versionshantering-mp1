e0307d468cf75e3dee2cd2b6de9a03b5c75bf39c
melsowole <82158719+melsowole@users.noreply.github.com>
2024-01-12 11:09:38 +0100

    darkmode check

--- modules/thememode.js
+++ modules/thememode.js
@@ -1,15 +1,24 @@
 export function handleDarkMode(event) {
-    event.preventDefault()
-	const checkboxEl = document.querySelector('#toggleMode').checked
+	const darkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
 
-	if(checkboxEl) {
-		document.documentElement.style.setProperty('--backgroundColor', 'linear-gradient(0deg,rgb(46, 56, 45) 0%,rgba(144, 144, 144, 0.667) 100%)')
-		document.documentElement.style.setProperty('--textColor', 'white')
-		console.log('off');
-	}
-	else {
-		document.documentElement.style.setProperty('--backgroundColor', 'linear-gradient(0deg,rgb(179, 212, 176) 0%,rgba(255, 255, 255, 0.667) 100%)')
-		document.documentElement.style.setProperty('--textColor', 'black')
-		console.log('on');
+	console.log(darkMode);
+
+	event.preventDefault();
+	const checkboxEl = document.querySelector("#toggleMode").checked;
+
+	if (checkboxEl) {
+		document.documentElement.style.setProperty(
+			"--backgroundColor",
+			"linear-gradient(0deg,rgb(46, 56, 45) 0%,rgba(144, 144, 144, 0.667) 100%)"
+		);
+		document.documentElement.style.setProperty("--textColor", "white");
+		console.log("off");
+	} else {
+		document.documentElement.style.setProperty(
+			"--backgroundColor",
+			"linear-gradient(0deg,rgb(179, 212, 176) 0%,rgba(255, 255, 255, 0.667) 100%)"
+		);
+		document.documentElement.style.setProperty("--textColor", "black");
+		console.log("on");
 	}
 }
